765a434c227ab35bb73dfb525520e0f45299776a
Bakyt Niiazov <bakytn@gmail.com>
2026-03-09 14:54:03 +0600

    site version bump

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.36.27';
+$SITE_VERSION = '1.36.28';
 
 $hostHeader = isset($_SERVER['HTTP_HOST']) ? strtolower(trim($_SERVER['HTTP_HOST'])) : '';
 $hostOnly = preg_replace('/:\d+$/', '', $hostHeader);
